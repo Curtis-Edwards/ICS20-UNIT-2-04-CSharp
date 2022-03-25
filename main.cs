4ffce87d50f27dc408e750be8963c92d78d8f454
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2022-03-25 21:21:31 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,24 @@
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This function accepts user input
+        int basse;
+        int height;
+
+        Console.WriteLine("Please enter the base and height.");
+        Console.WriteLine("");
+
+        Console.Write("base: ");
+        basse = Convert.ToInt32(Console.ReadLine());
+        Console.Write("height: ");
+        height = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("");
+        Console.WriteLine("Area: " + ( basse * height ) / 2 + "cm²");
+
+        Console.WriteLine("\nDone.");
+    }
+}
